a0533d64e841a006ee490cc072feac811c33ff3b
MosNes <7hekarl@gmail.com>
2023-08-11 12:40:03 -0400

    added model.ts, added submit handler and state management for todos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,36 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import InputField from './components/InputField';
+import {Todo} from './model';
 
 // React.FC is functional component
 const  App: React.FC =  () => {
 
+  //manages state of input field
   const [todo, setTodo] = useState<string>('');
+
+  //manages state of todo list
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  //handles submission of new Todo
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    //only change state if there's a todo in the input field
+    if (todo) {
+      //spread operator to copy todos array and add new todo, Date.now() to create unique id
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+      //clear input field
+      setTodo('');
+    }
+  }
+
+  console.log(todos);
   
   return (
     <div className="App">
       <span className="heading">Task Slayer</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -4,12 +4,22 @@ import './styles.css'
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({todo, setTodo}: Props) => {
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+
   return (
-    <form className="input">
-        <input type="input" placeholder="Enter a Task" className='inputBox'/>
+    <form className="input" onSubmit={handleAdd}>
+        <input 
+        type="input" 
+        placeholder="Enter a Task" 
+        className='inputBox'
+        value={todo}
+        onChange={
+          (e) => setTodo(e.target.value)
+        }
+        />
         <button className='inputSubmit' type='submit'>Add</button>
     </form>
   )

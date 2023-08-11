aab3d271695f903e3e838df53d217d970f97ba08
MosNes <7hekarl@gmail.com>
2023-08-11 12:59:38 -0400

    added inputRef to blur input on submit, added TodoList component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,6 +32,7 @@ const  App: React.FC =  () => {
     <div className="App">
       <span className="heading">Task Slayer</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList /> */}
     </div>
   );
 }
--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css'
 
 interface Props {
-    todo: string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>;
-    handleAdd: (e: React.FormEvent) => void;
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+
+  //create reference to the input field
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="input" onSubmit={handleAdd}>
-        <input 
-        type="input" 
-        placeholder="Enter a Task" 
+    <form className="input" onSubmit={
+      (e) => {
+        handleAdd(e);
+        //after the form is submitted, blur the input field so the background color goes back to normal
+        inputRef.current?.blur();
+      }
+    }>
+      <input
+        ref={inputRef}
+        type="input"
+        placeholder="Enter a Task"
         className='inputBox'
         value={todo}
         onChange={
           (e) => setTodo(e.target.value)
         }
-        />
-        <button className='inputSubmit' type='submit'>Add</button>
+      />
+      <button className='inputSubmit' type='submit'>Add</button>
     </form>
   )
 }

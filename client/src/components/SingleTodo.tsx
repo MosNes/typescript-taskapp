8570b0471371461d3d2fb6467cfc1522c1690a08
MosNes <7hekarl@gmail.com>
2023-08-11 13:31:23 -0400

    added icons and styling to SingleTodo component

--- client/src/components/SingleTodo.tsx
+++ client/src/components/SingleTodo.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { Todo } from '../model';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
+import { MdDone } from 'react-icons/md';
 
-type Props = {
-    todo: Todo,
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-}
+    type Props = {
+        todo: Todo,
+        todos: Todo[],
+        setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+    }
 
-const SingleTodo = ({todo, todos, setTodos}: Props) => {
-  return (
-    <form className='todosSingle'>
-        <span className='todosSingleText'>
-            {todo.todo}
-        </span>
-    </form>
-  )
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+    return (
+        <form className='todosSingle'>
+            <span className='todosSingleText'>
+                {todo.todo}
+            </span>
+            {/* div to hold icons */}
+            <div>
+                <span className="icon"><AiFillEdit /></span>
+                <span className="icon"><AiFillDelete /></span>
+                <span className="icon"><MdDone /></span>
+            </div>
+        </form>
+    )
 }
 
 export default SingleTodo
